12cb016dcaa9eb0d772c0fc8ec5ba6d3cd4395dd
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2020-06-19 14:14:38 +0300

    initial commit 🐱‍👤

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Vuepress First Look",
+  description: "Focusthen Vuepress First Look",
+  themeConfig: {
+    nav: [
+      {
+        text: "guides",
+        link: "guides/guide.html",
+      },
+    ],
+    sidebar: ["/", "/guides/guide.html"],
+  },
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello world form Vuepress
+
+## h2
+
+more text
+
+> markdown!
+
+:::tip
+Hooray
+:::
+
+[link somewhere](https://www.google.com)
